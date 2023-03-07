85a54005032ef3a949346c64fb9c5b31c3197671
wangwenjie <601905931@qq.com>
2023-03-07 10:12:02 +0800

    准备新增room数据库的管理

--- app/src/main/java/com/tuita/bookkeeping/utils/IORoomUtils.java
+++ app/src/main/java/com/tuita/bookkeeping/utils/IORoomUtils.java
@@ -1,5 +1,9 @@
 package com.tuita.bookkeeping.utils;
 
+import com.tuita.bookkeeping.room.entity.Bookkeeping;
+
+import java.util.List;
+
 /**
  * 静态工具类
  * 线程管理，用来异步操作room数据库
@@ -8,5 +12,9 @@ public class IORoomUtils {
 
     public static void insertNewsData(){
 
+    }
+
+    public static List<Bookkeeping> queryData(){
+        return null;
     }
 }
