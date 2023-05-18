b2176242fe5c807d2ef7a8b425c4277aae01378e
wangwenjie <601905931@qq.com>
2023-05-18 14:58:26 +0800

    添加谷歌的服务

--- app/src/main/java/com/tuita/bookkeeping/SuApplication.java
+++ app/src/main/java/com/tuita/bookkeeping/SuApplication.java
@@ -2,12 +2,25 @@ package com.tuita.bookkeeping;
 
 import android.app.Application;
 
-import com.tuita.bookkeeping.utils.RecordUtils;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
 
 public class SuApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
 //        RecordUtils.getInstance().initSimulationData();
+        initThirdSDK();
+    }
+
+    /**
+     * 初始化一些三方的SDK
+     */
+    private void initThirdSDK() {
+        //Google的FireBase，AppCheck组件
+        FirebaseApp.initializeApp(this);
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        firebaseAppCheck.installAppCheckProviderFactory(PlayIntegrityAppCheckProviderFactory.getInstance());
     }
 }
